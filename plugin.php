ea00e30594170948bbf32418caa84ab72ee26ee5
Willington Vega <wvega@wvega.com>
2019-09-21 16:05:57 -0500

    Add empty plugin

---
+++ plugin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The entry file for the Bulk Variation Price Editor for WooCommerce plugin.
+ *
+ * @since   1.0.0
+ * @author  Willington Vega <wvega@wvega.com>
+ *
+ * Plugin Name: Bulk Variation Price Editor for WooCommerce
+ * Description: Allows variations prices of a given product to be edited in bulk.
+ * Version: 1.0.0
+ * Requires PHP: 5.6.20
+ * Author: Willington Vega <wvega@wvega.com>
+ * Author URI: https://wvega.com
+ * License: GPL-3.0-or-later
+ * License URI: https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ * Text Domain: bulk-variation-price-editor-for-woocommerce
+ * Domain Path: /languages
+ */
