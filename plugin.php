3b6292aef89afbe4f78bc15331887ea5ecb50b0d
Willington Vega <wvega@wvega.com>
2019-09-21 17:41:24 -0500

    Add Edit Pries row action for Products

--- plugin.php
+++ plugin.php
@@ -16,3 +16,33 @@
  * Text Domain: bulk-variation-price-editor-for-woocommerce
  * Domain Path: /languages
  */
+
+
+function bvpe_plugins_loaded() {
+
+	add_filter( 'post_row_actions', 'bvpe_register_row_actions', 10, 2 );
+}
+add_action( 'plugins_loaded', 'bvpe_plugins_loaded' );
+
+
+function bvpe_register_row_actions( $actions, $post ) {
+
+	if ( 'product' === $post->post_type ) {
+
+		$query_args = [
+			'page'      => 'bulk-variation-price-editor',
+			'post_type' => $post->post_type,
+			'post'      => $post->ID,
+		];
+
+		$actions['edit-prices'] = sprintf(
+			'<a href="%1$s" rel="bookmark" aria-label="%2$s">%3$s</a>',
+			esc_url( add_query_arg( $query_args, admin_url( 'edit.php' ) ) ),
+			/* translators: %s: post title */
+			esc_attr( sprintf( __( 'Editar los precios de las variaciones de &#8220;%s&#8221;' ), get_the_title( $post ) ) ),
+			__( 'Editar precios' )
+		);
+	}
+
+	return $actions;
+}
