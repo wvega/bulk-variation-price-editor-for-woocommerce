e1ce37de738bac05a6b0eec4bc401c73f389e5ed
Willington Vega <wvega@wvega.com>
2019-09-21 18:23:55 -0500

    Add title, description, and submit button to admin page

--- templates/editor.tpl.php
+++ templates/editor.tpl.php
@@ -1,1 +1,11 @@
-Hola Meetup!
+<div class="wrap">
+	<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+
+	<p><?php echo esc_html( 'Puedes cambiar el precio para todos los colores de una misma talla modificando los valores en los campos que aparecen más abajo.', 'wss-edit-variations-prices-for-woocommerce' ); ?></p>
+
+	<form method="post">
+		<p class="submit">
+			<input class="button button-primary" type="submit" value="<?php echo esc_attr( 'Actualizar' ); ?>" />
+		</p>
+	</form>
+</div>
